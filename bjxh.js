2ce0c6a1b4909de6f38130f7a81eb20ae2d4a1b0
Sliverkiss <1393579810@qq.com>
2024-10-10 08:24:16 +0800

    Update bjxh.js

--- bjxh.js
+++ bjxh.js
@@ -1,7 +1,7 @@
 /*************************************
 
 [rewrite_local]
-^https:\/\/api-xh\.hsyuntai\.com\/hs-xh-single-web\/r\/173\/20002\/104 url script-response-body 
+^https:\/\/api-xh\.hsyuntai\.com\/hs-xh-single-web\/r\/173\/20002\/104 url script-response-body https://raw.githubusercontent.com/Sliverkiss/Test/refs/heads/main/bjxh.js
 
 [mitm]
 hostname = api-xh.hsyuntai.com
