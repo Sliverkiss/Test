c68bc7cc917246e804ca760d757b2bbdca30fe90
Sliverkiss <1393579810@qq.com>
2024-10-10 08:36:10 +0800

    Update bjxh.js

--- bjxh.js
+++ bjxh.js
@@ -1,15 +1,8 @@
 /*************************************
-
 [rewrite_local]
 ^https:\/\/api-xh\.hsyuntai\.com\/hs-xh-single-web\/r\/173\/20002\/104 url script-response-body https://raw.githubusercontent.com/Sliverkiss/Test/refs/heads/main/bjxh.js
 
 [mitm]
 hostname = api-xh.hsyuntai.com
-
 *************************************/
-
-var chxm1023 = JSON.parse($response.body);
-
-chxm1023.data.forEach(e => e.resNo = 0);
-
-$done({ body: JSON.stringify(chxm1023) });
+$done({ body: JSON.stringify($response.body.replace(/"resNo":\s*\d+/g, '"resNo": 1')) });
