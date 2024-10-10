a169a0ff8f80ef94cd210f37e08cc67d13c833ea
Sliverkiss <1393579810@qq.com>
2024-10-10 08:23:59 +0800

    Create bjxh.js

---
+++ bjxh.js
@@ -0,0 +1,15 @@
+/*************************************
+
+[rewrite_local]
+^https:\/\/api-xh\.hsyuntai\.com\/hs-xh-single-web\/r\/173\/20002\/104 url script-response-body 
+
+[mitm]
+hostname = api-xh.hsyuntai.com
+
+*************************************/
+
+var chxm1023 = JSON.parse($response.body);
+
+chxm1023.data.forEach(e => e.resNo = 0);
+
+$done({ body: JSON.stringify(chxm1023) });
